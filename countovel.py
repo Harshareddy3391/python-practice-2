b502f0aae71817303faeaa600d5c129486db3f27
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-10 23:40:07 +0530

    count function

--- countovel.py
+++ countovel.py
@@ -1,17 +1,38 @@
-string_val="hello world"
+#string_val="hello world"
 
 #print(string_val.count('o''e''i''u''a'))
 
 #print(type(string_val))
 
 
-ovl="aeiou"
-
+#ovl="aeiou"
+"""
 d=0
 
 for i in string_val:
     if i in ovl:
       d += 1
 
-print(d)
+print(d)"""
+
+
+
+
+
+#by using function
+
+
+def ovel_count(a):
+   
+   count=0
+
+   st=str(a)
+   for i in st:
+     if i.lower() in "aioue":
+            count+=1
+   return print(count)
+   
+ovel_count("harsha")
+
+     
      
